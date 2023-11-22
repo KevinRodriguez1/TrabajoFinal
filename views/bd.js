6d87a16ed26358a3df8520ae234a9fef4a1327a1
Kevin Rodriguez <kevin.rodriguez@itscordoba.edu.ar>
2023-11-22 00:39:59 -0300

    Consulta pod SQL

---
+++ views/bd.js
@@ -0,0 +1,58 @@
+const mysql  = requiere('mysql2');
+const bd = mysql.createConnection({
+    host:'localhost',
+    user:'root ',
+    password:'1234 '
+
+});
+
+//Conectarno al servidos
+db.connect((err) => {
+    if (err){
+        console.log("Error en la conexion al servidor");
+        return;
+    } 
+
+    /// Verificar si existe la base de  datos
+
+    bd.query("CREATE DATABASE IF  NOT EXITS",(err) => {
+        if (err){
+            console.log("Error en la creacion de la base de datos");
+            return; 
+        }
+                console.log("Base de datos creada");
+    });
+
+
+    // Seleccionar base de datos
+    bd.query("USE personas",(err) => {
+        if (err){
+            console.log("Error en la creacion de la base de datos");
+            return; 
+        }
+        console.log("Base de datos creada");
+   });
+
+
+    // Verificar si exite la tabla persona
+    const createTableSQL = ' 
+        CREATE TABLE IF NOT EXITS persona(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nombre VARCHAR(50),
+            apellido VARCHAR(50),
+            email VARCHAR(255)
+        )
+    
+    
+    ';
+    bd.query(createTableSQL, (err) = {
+        if (err){
+            console.log("Error en la creacion de la base de datos");
+            return; 
+        }
+
+    });
+
+});
+
+module.exports = bd;
